463c2353d1444b941734732425f368e0876bee6e
Riza Khan <riza.khan@appnovation.com>
2024-03-19 09:24:01 -0400

    adding jest

--- plugins/neotest.lua
+++ plugins/neotest.lua
@@ -7,11 +7,18 @@ return {
     "antoinemadec/FixCursorHold.nvim",
     "nvim-treesitter/nvim-treesitter",
     "marilari88/neotest-vitest",
+    "nvim-neotest/neotest-jest",
   },
   config = function()
     require("neotest").setup {
       adapters = {
         require "neotest-vitest",
+        require "neotest-jest" {
+          jestCommand = "npm test --",
+          jestConfigFile = "jest.config.ts",
+          env = { CI = true },
+          cwd = function(path) return vim.fn.getcwd() end,
+        },
       },
     }
   end,
